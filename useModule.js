014181f1c6d06985d9522b8ce9229573b320a54c
Ankit <ankitpandey12348409@gmail.com>
2026-09-01 10:16:07 +0530

    updating files

--- useModule.js
+++ useModule.js
@@ -1,8 +1,12 @@
 const isEven = require("./modules/isEven");
+const logger = require("./modules/logger");
+
+logger("Application started.....");
 
 console.log("10 is even:", isEven(10));
 console.log("7 is even:", isEven(7));
 
+logger("Application finished....");
 
 
-// run: node useModule.js
+// run :node useModule.js
